8a8ab75f04b120ddf029386be2c9b70d0125ef72
Payal Kandlur <pkandlur@teksystems.com>
2020-07-22 16:26:33 +0530

    Initial Commit

---
+++ trackStep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trackStep
+//
+//  Created by Payal Kandlur on 22/07/20.
+//  Copyright © 2020 Payal Kandlur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ trackStepTests/trackStepTests.swift
@@ -0,0 +1,34 @@
+//
+//  trackStepTests.swift
+//  trackStepTests
+//
+//  Created by Payal Kandlur on 22/07/20.
+//  Copyright © 2020 Payal Kandlur. All rights reserved.
+//
+
+import XCTest
+@testable import trackStep
+
+class trackStepTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
